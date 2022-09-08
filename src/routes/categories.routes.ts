525ba831e6b7557f8a4cfa87b826340af30109d4
ThiagoHenr <tiagohenrriquebuenodeoliveira@gmail.com>
2022-09-08 11:26:21 -0300

    Separação em módulos.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { CreateCategoryService } from "../services/CreateCategoryService";
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../modules/services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/repositories/CategoriesRepository";
 
 
 const categoriesRoutes = Router()
