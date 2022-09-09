c63c347f004ff479cfc3cde03d48067b6ae2d707
ThiagoHenr <tiagohenrriquebuenodeoliveira@gmail.com>
2022-09-09 10:40:03 -0300

    Lendo os arquivos importados.

--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,9 +1,18 @@
-
+import { parse } from "csv-parse"
+import fs from "fs"
 
 class ImportCategoriesUseCase {
 
     execute(file: Express.Multer.File):void {
-        console.log(file)
+        const stream =fs.createReadStream(file.path)
+
+        const parseFile = parse()
+
+        stream.pipe(parseFile)
+
+        parseFile.on("data", async (line) => {
+            console.log(line)
+        })
     }
 }
 
