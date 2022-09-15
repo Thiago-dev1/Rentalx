89b58f32e367e37e2b88ad507691a26932d7268c
Thiago Henrique <tiago@DESKTOP-P5HO0PT.localdomain>
2022-09-15 09:50:01 -0300

    Certo docker.

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,21 @@
-import { createConnection } from "typeorm"
+import { DataSource } from "typeorm"
 
-createConnection()
+const AppDataSource  = new DataSource({
+    type: "postgres",
+    host: "database_ignite",
+    port: 5432,
+    username: "docker",
+    password: "ignite",
+    database: "rentx",
+    entities: [],
+    migrations: ['./migrations/'],
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
