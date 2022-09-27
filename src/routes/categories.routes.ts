b84190abc3b84bc408052fea195ae5aff940c260
Thiago Henrique <tiago@DESKTOP-P5HO0PT.localdomain>
2022-09-27 14:36:46 -0300

    Admin.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,6 +5,8 @@ import { CreateCategoryController } from "../modules/cars/useCases/createCategor
 import { ListCategoriesController }  from "../modules/cars/useCases/listCategories/ListCategoiresController";
 import { ImportCategoriesController } from "../modules/cars/useCases/importCategories/ImportCategoriesController";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
+import { ensureAdmin } from "../middlewares/ensureAdmin"
 
 const categoriesRoutes = Router()
 const upload = multer({
@@ -15,10 +17,10 @@ const createCategoryController = new CreateCategoryController()
 const importCategoriesController = new ImportCategoriesController()
 const listCategoriesController = new ListCategoriesController()
 
-categoriesRoutes.post("/", createCategoryController.handle)
+categoriesRoutes.post("/", ensureAuthenticated, ensureAdmin, createCategoryController.handle)
 
 categoriesRoutes.get("/", listCategoriesController.handle)
 
-categoriesRoutes.post("/import", upload.single("file"), importCategoriesController.handle) 
+categoriesRoutes.post("/import",ensureAuthenticated, ensureAdmin,  upload.single("file"), importCategoriesController.handle) 
 
 export { categoriesRoutes }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,17 +1,18 @@
 import { Router } from "express";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 import { CreateSpeficificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController"
 import { ListSpecificationsController } from "../modules/cars/useCases/listSpecifications/ListSpecificationsController";
+
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
+import { ensureAdmin } from "../middlewares/ensureAdmin"
 
 const specificationsRoutes = Router()
 
 const createSpeficificationController = new CreateSpeficificationController()
 const listSpecificationsController  = new ListSpecificationsController()
 
-specificationsRoutes.use(ensureAuthenticated)
 
-specificationsRoutes.post("/", createSpeficificationController.handle)
+specificationsRoutes.post("/", ensureAuthenticated, ensureAdmin, createSpeficificationController.handle)
 
 specificationsRoutes.get("/", listSpecificationsController.handle)
 
