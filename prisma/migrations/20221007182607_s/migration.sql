eefb8dbbed28000e6a3fba701b5c9c8d16c84323
Thiago Henrique <tiago@DESKTOP-P5HO0PT.localdomain>
2022-10-07 16:10:05 -0300

    bd

---
+++ prisma/migrations/20221007182607_s/migration.sql
@@ -0,0 +1,104 @@
+/*
+  Warnings:
+
+  - You are about to drop the `categories` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `specifications` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "users_email_key";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "categories";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "specifications";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "users";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Specification" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatar" TEXT,
+    "driverLicense" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_cars" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "dailyRate" INTEGER NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
+    "license_plate" TEXT NOT NULL,
+    "fine_amount" INTEGER NOT NULL,
+    "brand" TEXT NOT NULL,
+    "category_id" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "cars_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category" ("id") ON DELETE SET NULL ON UPDATE SET NULL
+);
+INSERT INTO "new_cars" ("available", "brand", "category_id", "createdAt", "dailyRate", "description", "fine_amount", "id", "license_plate", "name") SELECT "available", "brand", "category_id", "createdAt", "dailyRate", "description", "fine_amount", "id", "license_plate", "name" FROM "cars";
+DROP TABLE "cars";
+ALTER TABLE "new_cars" RENAME TO "cars";
+CREATE TABLE "new_rentals" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
+    "car_id" TEXT NOT NULL,
+    "stard_dte" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "end_date" DATETIME,
+    "expected_return_date" DATETIME NOT NULL,
+    "total" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "rentals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "rentals_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "cars" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_rentals" ("car_id", "createdAt", "end_date", "expected_return_date", "id", "stard_dte", "total", "updatedAt", "user_id") SELECT "car_id", "createdAt", "end_date", "expected_return_date", "id", "stard_dte", "total", "updatedAt", "user_id" FROM "rentals";
+DROP TABLE "rentals";
+ALTER TABLE "new_rentals" RENAME TO "rentals";
+CREATE TABLE "new_specifications_cars" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "car_id" TEXT NOT NULL,
+    "specification_id" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "specifications_cars_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "cars" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "specifications_cars_specification_id_fkey" FOREIGN KEY ("specification_id") REFERENCES "Specification" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_specifications_cars" ("car_id", "createdAt", "id", "specification_id") SELECT "car_id", "createdAt", "id", "specification_id" FROM "specifications_cars";
+DROP TABLE "specifications_cars";
+ALTER TABLE "new_specifications_cars" RENAME TO "specifications_cars";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
